14e124f4bfa4ffe07f87bccb3b0a58e061f9b366
Rajko Albrecht <ral@alwins-world.de>
2016-02-14 11:35:41 +0100

    binary name changed

--- scripts/start.sh
+++ scripts/start.sh
@@ -38,9 +38,8 @@ done
 # Run the teamspeak server
 export LD_LIBRARY_PATH=/opt/teamspeak
 
-
 if [ ! -f /data/ts3server.ini ]; then
     EXTRA="createinifile=1"
 fi
-/opt/teamspeak/ts3server_linux_amd64 logpath=/data/logs/ dbsqlpath=/opt/teamspeak/sql/ ${EXTRA}
+/opt/teamspeak/ts3server logpath=/data/logs/ dbsqlpath=/opt/teamspeak/sql/ ${EXTRA}
 
